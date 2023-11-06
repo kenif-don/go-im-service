35ad34316718c0575ca83e5874b1ed849087e199
wqq <wqq.master@gmail.com>
2023-11-06 15:23:53 +0800

    1. Makefile 重构

--- build/android/bean/src/androidTest/java/com/master/world/android/ExampleInstrumentedTest.java
+++ build/android/bean/src/androidTest/java/com/master/world/android/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.itmsoft.im.android.test", appContext.getPackageName());
+        assertEquals("com.master.world.android", appContext.getPackageName());
     }
 }
