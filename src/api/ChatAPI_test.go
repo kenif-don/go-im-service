3933eaaa84868c5490e77a8cdab919cc0c0eaa26
wqq <wqq.master@gmail.com>
2023-11-16 21:09:18 +0800

    1. 添加消息发送事件

--- src/api/ChatAPI_test.go
+++ src/api/ChatAPI_test.go
@@ -29,7 +29,7 @@ func TestSendMsg(t *testing.T) {
 	TestOpenChat(t)
 	contentObj := &entity.MessageData{
 		Type:    1,
-		Content: "23423",
+		Content: "111",
 	}
 	content, _ := util.Obj2Str(contentObj)
 	oldReq := &api.ChatReq{
