3b3ffe0c8edef7d6bd46e2a08ddf30613a93814b
wqq <wqq.master@gmail.com>
2023-12-27 16:37:37 +0800

    1. 发送消息日志

--- src/api/ChatAPI_test.go
+++ src/api/ChatAPI_test.go
@@ -3,6 +3,7 @@ package api
 import (
 	api "IM-Service/build/generated/service/v1"
 	"IM-Service/src/configs/log"
+	"IM-Service/src/util"
 	"google.golang.org/protobuf/proto"
 	"testing"
 	"time"
@@ -28,8 +29,8 @@ func TestDecrypt(t *testing.T) {
 }
 func TestOpenChat(t *testing.T) {
 	oldReq := &api.ChatReq{
-		Type:   "friend",
-		Target: 21,
+		Type:   "group",
+		Target: 22,
 	}
 	req, _ := proto.Marshal(oldReq)
 	resp := OpenChat(req)
@@ -47,29 +48,29 @@ func TestSendMsg(t *testing.T) {
 	//TestGetChats(t)
 	//time.Sleep(time.Second * 2)
 	TestOpenChat(t)
-	//time.Sleep(time.Second * 2)
+	time.Sleep(time.Second * 2)
 	TestGetMsgs(t)
-	//log.Errorf("exception:%v", nil)
-	//contentObj := &api.MessageData{
-	//	//Type:    2,
-	//	//Content: "C:\\Users\\Administrator\\Desktop\\logo.png",
-	//	//Content: "C:\\Users\\Administrator\\Desktop\\b_be930378be7919df8057ce403e1b4d3e.gif",
-	//	Type:    1,
-	//	Content: "成交价啊山莨菪碱扫",
-	//}
-	//oldReq := &api.ChatReq{
-	//	Type:    "friend",
-	//	Target:  1,
-	//	No:      util.Uint642Str(uint64(time.Now().UnixMilli())),
-	//	Content: contentObj,
-	//}
-	//req, _ := proto.Marshal(oldReq)
-	//resp := SendMsg(req)
-	//result := &api.ResultDTOResp{}
-	//err := proto.Unmarshal(resp, result)
-	//if err != nil {
-	//	log.Error(err)
-	//}
+	log.Errorf("exception:%v", nil)
+	contentObj := &api.MessageData{
+		//Type:    2,
+		//Content: "C:\\Users\\Administrator\\Desktop\\logo.png",
+		//Content: "C:\\Users\\Administrator\\Desktop\\b_be930378be7919df8057ce403e1b4d3e.gif",
+		Type:    1,
+		Content: "成交价啊山莨菪碱扫",
+	}
+	oldReq := &api.ChatReq{
+		Type:    "group",
+		Target:  22,
+		No:      util.Uint642Str(uint64(time.Now().UnixMilli())),
+		Content: contentObj,
+	}
+	req, _ := proto.Marshal(oldReq)
+	resp := SendMsg(req)
+	result := &api.ResultDTOResp{}
+	err := proto.Unmarshal(resp, result)
+	if err != nil {
+		log.Error(err)
+	}
 	time.Sleep(time.Hour)
 }
 func TestGetChats(t *testing.T) {
