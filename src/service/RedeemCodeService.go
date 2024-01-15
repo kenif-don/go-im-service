85f431f180f08386de9e0bfc8efb069dc67d60c6
wqq <wqq.master@gmail.com>
2024-01-15 19:25:55 +0800

    1. 增加验证

--- src/service/RedeemCodeService.go
+++ src/service/RedeemCodeService.go
@@ -14,6 +14,9 @@ func NewRedeemCodeService() *RedeemCodeService {
 
 // Create 钱兑码
 func (_self *RedeemCodeService) Create(money string) (string, *utils.Error) {
+	if money == "" {
+		return "", log.WithError(utils.ERR_PARAM_PARSE)
+	}
 	resultDTO, err := Post("/api/redeem-code/create", map[string]string{"money": money})
 	if err != nil {
 		return "", log.WithError(err)
@@ -26,6 +29,9 @@ func (_self *RedeemCodeService) Create(money string) (string, *utils.Error) {
 
 // Exchange 码兑钱
 func (_self *RedeemCodeService) Exchange(code string) *utils.Error {
+	if code == "" {
+		return log.WithError(utils.ERR_PARAM_PARSE)
+	}
 	_, err := Post("/api/redeem-code/exchange", map[string]string{"code": strings.TrimSpace(code)})
 	if err != nil {
 		return log.WithError(err)
@@ -47,6 +53,9 @@ func (_self *RedeemCodeService) Paging(page, pageSize int) (string, *utils.Error
 
 // SelectOne 获取兑换记录
 func (_self *RedeemCodeService) SelectOne(code string) (string, *utils.Error) {
+	if code == "" {
+		return "", log.WithError(utils.ERR_PARAM_PARSE)
+	}
 	resultDTO, err := Post("/api/redeem-code/selectOne", map[string]string{"code": strings.TrimSpace(code)})
 	if err != nil {
 		return "", log.WithError(err)
